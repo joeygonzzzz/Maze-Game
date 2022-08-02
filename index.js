4c2d42b8527576811e7e987afb604f710f2a0721
joeygonzzzz <91030987+joeygonzzzz@users.noreply.github.com>
2022-08-01 21:39:16 -0500

    setting up cells

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const {
     Bodies,
 } = Matter;
 
+const cells = 3;
 const width = 600;
 const height = 600;
 
@@ -36,16 +37,23 @@ World.add(world, walls);
 
 // Maze generation
 
-const grid = Array(3)
+const grid = Array(cells)
     .fill(null)
-    .map(() => Array(3).fill(false));
+    .map(() => Array(cells).fill(false));
 
-const verticals = Array(3)
+const verticals = Array(cells)
     .fill(null)
-    .map(() => Array(2).fill(false))
+    .map(() => Array(cells - 1).fill(false))
 
-    const horizontals = Array(2)
+    const horizontals = Array(cells - 1)
     .fill(null)
-    .map(() => Array(3).fill(false))
+    .map(() => Array(cells).fill(false))
 
-console.log(horizontals)
+const startRow = Math.floor(Math.random() * cells);
+const startColumn = Math.floor(Math.random() * cells);
+
+const stepThroughCell = (row, column) => {
+
+};
+
+stepThroughCell(startRow, startColumn)
