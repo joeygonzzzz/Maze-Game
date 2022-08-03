bc4b2ea1429dfd4e1625d9fb19580b8d73240a58
joeygonzzzz <91030987+joeygonzzzz@users.noreply.github.com>
2022-08-02 19:36:13 -0500

    ball

--- index.js
+++ index.js
@@ -29,10 +29,10 @@ Runner.run(Runner.create(), engine);
 
 // Walls
 const walls = [
-    Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true}),
-    Bodies.rectangle(width / 2, height, width, 40, { isStatic: true}),
-    Bodies.rectangle(0, height / 2, 40, height, { isStatic: true}),
-    Bodies.rectangle(width, width / 2, 40, height, { isStatic: true})
+    Bodies.rectangle(width / 2, 0, width, 2, { isStatic: true}),
+    Bodies.rectangle(width / 2, height, width, 2, { isStatic: true}),
+    Bodies.rectangle(0, height / 2, 2, height, { isStatic: true}),
+    Bodies.rectangle(width, width / 2, 2, height, { isStatic: true})
 ];
 
 World.add(world, walls);
@@ -129,7 +129,7 @@ horizontals.forEach((row, rowIndex) => {
             columnIndex * unitLength + unitLength / 2,
             rowIndex * unitLength + unitLength,
             unitLength, 
-            10,
+            5,
             {
                 isStatic: true
             }
@@ -147,7 +147,7 @@ verticals.forEach((row, rowIndex) => {
         const wall = Bodies.rectangle(
             columnIndex * unitLength + unitLength,
             rowIndex * unitLength + unitLength / 2,
-            10,
+            5,
             unitLength,
             {
                 isStatic: true
@@ -156,3 +156,25 @@ verticals.forEach((row, rowIndex) => {
         World.add(world, wall);
     });
 });
+
+
+//Goal
+const goal = Bodies.rectangle(
+    width - unitLength / 2,
+    height - unitLength / 2,
+    unitLength * .7,
+    unitLength * .7,
+    {
+        isStatic: true
+    }
+);
+World.add(world, goal);
+
+// Ball
+
+const ball = Bodies.circle(
+    unitLength / 2,
+    unitLength / 2,
+    unitLength / 4
+);
+World.add(world, ball);
